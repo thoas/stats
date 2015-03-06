033e6c19bb81cabebf13d25a4a820135b59c2dc9
Florent Messa <florent.messa@gmail.com>
2015-03-06 14:38:33 +0100

    Add basic unittests

--- stats_test.go
+++ stats_test.go
@@ -1,1 +1,25 @@
 package stats
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+var testHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	w.Write([]byte("bar"))
+})
+
+func TestSimple(t *testing.T) {
+	s := New()
+
+	res := httptest.NewRecorder()
+
+	req, _ := http.NewRequest("GET", "http://example.com/foo", nil)
+
+	s.Handler(testHandler).ServeHTTP(res, req)
+
+	assert.Equal(t, s.ResponseCounts, map[string]int{"200": 1})
+}
