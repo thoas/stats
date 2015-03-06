bffac3595325a52348ea7e0ba0e66d331ac5199c
Florent Messa <florent.messa@gmail.com>
2015-03-06 15:27:11 +0100

    Add more unittests

--- stats_test.go
+++ stats_test.go
@@ -1,6 +1,7 @@
 package stats
 
 import (
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
@@ -21,5 +22,41 @@ func TestSimple(t *testing.T) {
 
 	s.Handler(testHandler).ServeHTTP(res, req)
 
+	assert.Equal(t, res.Code, 200)
 	assert.Equal(t, s.ResponseCounts, map[string]int{"200": 1})
+}
+
+func TestGetStats(t *testing.T) {
+	s := New()
+
+	var stats = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		stats := s.GetStats()
+
+		b, _ := json.Marshal(stats)
+
+		w.Write(b)
+		w.WriteHeader(200)
+		w.Header().Add("Content-Type", "application/json")
+	})
+
+	res := httptest.NewRecorder()
+
+	req, _ := http.NewRequest("GET", "http://example.com/foo", nil)
+
+	s.Handler(testHandler).ServeHTTP(res, req)
+
+	res = httptest.NewRecorder()
+
+	s.Handler(stats).ServeHTTP(res, req)
+
+	assert.Equal(t, res.Header().Get("Content-Type"), "application/json")
+
+	var data map[string]interface{}
+
+	err := json.Unmarshal(res.Body.Bytes(), &data)
+
+	assert.Nil(t, err)
+
+	assert.Equal(t, data["TotalCount"].(float64), 1)
 }
